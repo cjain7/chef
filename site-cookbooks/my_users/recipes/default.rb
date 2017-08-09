cf0e2fec71954e9b23c482444d270fcef4da9ab7
Andy Fang <andyfang.dz@gmail.com>
2017-08-09 00:35:25 -0700

    Format files

--- site-cookbooks/my_users/recipes/default.rb
+++ site-cookbooks/my_users/recipes/default.rb
@@ -5,20 +5,19 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 users_manage "sysops" do
-    group_id 2147
-    action [ :remove, :create ]
+  group_id 2147
+  action [ :remove, :create ]
 end
 
 users_manage "docker" do
-    group_id 2148
-    action [ :remove, :create ]
+  group_id 2148
+  action [ :remove, :create ]
 end
 
-
 %w( sysadmin sudo ).each do |group|
-    users_manage group do
-        action [ :remove, :create ]
-    end
+  users_manage group do
+    action [ :remove, :create ]
+  end
 end
 
 node.default['authorization']['sudo']['groups'] = [ "sysadmin", "sysops", "sudo" ]
